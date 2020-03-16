82d28e5840e4b19581040c3ba088cd525a7ebe73
XiangZ-416 <zx04161313@163.com>
2020-03-16 10:55:52 +0800

    学习记录

---
+++ JDBC/src/Pakg_01_TestStatementCRUD/StatementTest.java
@@ -0,0 +1,122 @@
+package Pakg_01_TestStatementCRUD;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class StatementTest {
+
+	// 使用Statement的弊端：需要拼写sql语句，并且存在SQL注入的问题
+	// SQL注入：利用某些系统没有对用户输入的数据进行充分的检查，而在用户输入数据中注入非法的 SQL 语句段
+	//		或命令(如：SELECT user, password FROM user_table WHERE user='a' OR 1 = ' AND password = ' OR '1' =
+	//		'1') ，从而利用系统的 SQL 引擎完成恶意行为的做法。
+	// 如何避免sql注入：用PreparedStatement
+	@Test
+	public void testLogin() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("请输入用户名：");
+		String user = scanner.next();
+		System.out.println("请输入密码：");
+		String password = scanner.next();
+
+		String sql = "SELECT user, password FROM user_table WHERE user = '"+ user +"' AND password = '"+ password +"'";
+
+		User returnUser = get(sql, User.class);
+		if (returnUser != null) {
+			System.out.println("登陆成功");
+		}else {
+			System.out.println("用户名不存在或密码错误");
+		}
+	}
+
+	// 使用Statement实现对数据表的查询操作
+	public <T> T get(String sql, Class<T> clazz) {
+		T t = null;
+
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			// 1.加载配置文件
+			InputStream is = StatementTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
+			Properties pros = new Properties();
+			pros.load(is);
+
+			// 2.读取配置信息
+			String user = pros.getProperty("user");
+			String password = pros.getProperty("password");
+			String url = pros.getProperty("url");
+			String driverClass = pros.getProperty("driverClass");
+
+			// 3.加载驱动
+			Class.forName(driverClass);
+
+			// 4.获取连接
+			conn = DriverManager.getConnection(url, user, password);
+
+			st = conn.createStatement();
+
+			rs = st.executeQuery(sql);
+
+			// 获取结果集的元数据
+			ResultSetMetaData rsmd = rs.getMetaData();
+
+			// 获取结果集的列数
+			int columnCount = rsmd.getColumnCount();
+
+			if (rs.next()) {
+
+				t = clazz.newInstance();
+
+				for (int i = 0; i < columnCount; i++) {
+					// //1. 获取列的名称
+					// String columnName = rsmd.getColumnName(i+1);
+
+					// 1. 获取列的别名
+					String columnName = rsmd.getColumnLabel(i + 1);
+
+					// 2. 根据列名获取对应数据表中的数据
+					Object columnVal = rs.getObject(columnName);
+
+					// 3. 将数据表中得到的数据，封装进对象
+					Field field = clazz.getDeclaredField(columnName);
+					field.setAccessible(true);
+					field.set(t, columnVal);
+				}
+				return t;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 关闭资源
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (st != null) {
+				try {
+					st.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+}
--- 剑指Offer题解/src/main26.java
+++ 剑指Offer题解/src/main26.java
@@ -1,6 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * 题目描述
+ * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，
+ * 另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
+ *（注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
  * @author ZX
  * @date 2020/3/13 - 21:20
  */
 public class main26 {
+    public static class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+
+    /**
+     * 方法1：利用map<RandomListNode, RandomListNode>
+     *          1.pHead作为key，复制节点作为value
+     *          2.设置复制链表各个节点的next和random
+     * 时间复杂度：O(N)
+     * 额外空间复杂度：O(N)
+     * @param pHead
+     * @return
+     */
+    public RandomListNode Clone1(RandomListNode pHead) {
+        // 1.pHead作为key，复制节点作为value
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        RandomListNode Cur = pHead;
+        while (Cur != null) {
+            map.put(Cur, new RandomListNode(Cur.label));
+            Cur = Cur.next;
+        }
+
+        Cur = pHead;
+        // 2.设置复制链表各个节点的next和random
+        while (Cur != null) {
+            map.get(Cur).next = map.get(Cur.next);
+            map.get(Cur).random = map.get(Cur.random);
+            Cur = Cur.next;
+        }
+        return map.get(pHead);
+    }
+
+    /**
+     * 方法2：
+     * 时间复杂度：O(N)
+     * 额外空间复杂度：O(1)
+     * @param pHead
+     * @return
+     */
+    public RandomListNode Clone2(RandomListNode pHead) {
+
+        return null;
+    }
 }
