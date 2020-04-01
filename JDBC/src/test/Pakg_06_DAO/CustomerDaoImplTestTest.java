f1b4e00d9e155a9be27015371a2d9d3f0b84868d
XiangZ-416 <zx04161313@163.com>
2020-04-02 00:12:49 +0800

    学习记录

---
+++ JDBC/src/test/Pakg_06_DAO/CustomerDaoImplTestTest.java
@@ -0,0 +1,25 @@
+package test.Pakg_06_DAO; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* CustomerDaoImplTest Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>3月 23, 2020</pre> 
+* @version 1.0 
+*/ 
+public class CustomerDaoImplTestTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+
+} 
